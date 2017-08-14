a7cee864a41019566ae1b16342691c01080805de
Jamieson <jwebking@gmail.com>
2017-08-14 15:47:01 -0400

    Day 11

---
+++ destroyer.js
@@ -0,0 +1,13 @@
+function destroyer(arr) {
+
+ var args = Array.from(arguments).slice(1);
+  return arr.filter(function(num){
+    return !args.includes(num);// if num is not in args, return those
+  });
+
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. 
+// Remove all elements from the initial array that are of the same value as these arguments.
---
+++ mutations.js
@@ -0,0 +1,21 @@
+function mutation(arr) {
+
+    var zero = arr[0].toString().toLowerCase().split('');
+    var one = arr[1].toString().toLowerCase().split('');
+
+    return one.every(function (letter) {
+        return zero.indexOf(letter) !== -1;
+         //indexOf returns the first index of where a letter is, or -1 if it's not found, 
+         //that's why you have to use that rather than true/false
+    });
+}
+
+mutation(["floor", "for"]);
+
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
